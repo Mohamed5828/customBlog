7b4c1ca4509b8cdc8b47c852cb1acb5696cc00bb
mohamed <mohamed5828@github.com>
2023-04-06 17:17:13 +0200

    tesing

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const connectDB = async () => {
       console.log(err);
     });
 };
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
