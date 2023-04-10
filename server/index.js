d12fdf4fee268b637810ddd82ea05cec6d5b0f20
mohamed <mohamed5828@github.com>
2023-04-10 09:43:52 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 // };
 
 const app = express();
-app.use(express.urlencoded({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "25mb" }));
 app.use(bodyParser.json());
 app.use("/", routesHandler);
